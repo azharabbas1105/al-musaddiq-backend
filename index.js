dfce95c72da3c79daecc564f52f9b17e9b9cb1b6
azhar <azhar@4iisolutions.com>
2023-12-12 19:20:56 +0500

    fix cors issue

--- index.js
+++ index.js
@@ -31,22 +31,22 @@ const app = express();
 
 // app.use(cors(corsOpts));
 
-// app.use(cors({
-//   origin: ['http://localhost:4200'],
-//   "methods": "PUT,GET,POST,DELETE",
-//   "preflightContinue": false,
-//   "optionsSuccessStatus": 204,
-//   credentials: true
-// }));
 app.use(cors({
-  origin: '*'
+  origin: '*',
+  "methods": "PUT,GET,POST,DELETE",
+  // "preflightContinue": false,
+  // "optionsSuccessStatus": 204,
+  credentials: true
 }));
-// app.use(function(req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin: *");
-//   res.setHeader("Access-Control-Allow-Methods: PUT,GET,POST,DELETE");
-//   res.setHeader("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+// app.use(cors({
+//   origin: '*'
+// }));
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin: *");
+  res.setHeader("Access-Control-Allow-Methods: PUT,GET,POST,DELETE");
+  res.setHeader("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 /* for Angular Client (withCredentials) */
 // app.use(
